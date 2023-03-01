633b5ddfc2ef6abb87cb63ed934d6f2d79f3cb10
Andy Qin <andyqin17@gmail.com>
2023-03-01 10:38:21 -0800

    Add React ui skeleton

---
+++ ui/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Chart from './Chart';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Chart />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ui/src/Chart.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Chart = () => {
+  return (
+    <>
+      <p>
+        Chart!
+      </p>
+    </>
+  )
+}
+
+export default Chart;
